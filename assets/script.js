99c8da602db762358e57c814551cb6724c4ff8d9
Joseph Baek <josephbaek97@gmail.com>
2023-03-20 10:23:41 -0700

    Added functionality for getting the current day to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,6 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var advancedFormat = require('dayjs/plugin/advancedFormat');
-dayjs().extend(advancedFormat);
 // $(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -15,23 +13,31 @@ dayjs().extend(advancedFormat);
         console.log(key);
         localStorage.setItem(key, "");
     };
-    var saveButton = $(".saveBtn");
+    var saveButton = $('.saveBtn');
     saveButton.on('click', save);
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
+    function currentHour () {
+        var hour = dayjs().format('HH');
+        console.log(hour);
+    } 
+
+    currentHour;
 
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    //Current Date
     function currentDate () {
-        var date = document.getElementById("#currentDay");
+        var date = $('#currentDay');
         var today = dayjs().format('dddd, MMM Do');
-        console.log(today);
+        date.html(today);
+        console.log(date);
     };
     
     currentDate();
